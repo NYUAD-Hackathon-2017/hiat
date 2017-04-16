e3ff8fc8d1ea9620a414b140fa6ec4b1405d8e48
ilievski <vladimir.ilievski@epfl.ch>
2017-04-16 07:53:46 +0200

    Fixing feedback messages

--- send_sms.py
+++ send_sms.py
@@ -6,13 +6,14 @@ import os
 from twilio.rest import Client
 from twilio.twiml.voice_response import VoiceResponse
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 ACCOUNT_SID = 'ACcefa2306ccdba981522a00c621e602ab'
 AUTH_TOKEN = 'ba3216346af36cf72ad0fb48e45e5b8e'
-PHONE = '+971556383129'
-CUSTOMER = '+14158573334'
+PHONE = '+41794477448'
+CUSTOMER = '+5491160190584'
 
 JOB_REQUEST = """لقد وجدنا فرصة عمل تناسبك!
 المكان: منطقة كوجاتيبي، شارع مختار بيك، بناء رقم ١-٢٠١ / اسطنبول، تركيا
@@ -24,27 +25,35 @@ JOB_DETAILS = """التفاصيل:
 إصلاح مغسلة ماء ليست تعمل جيدا، لا يخرج منها ماء أبدا ويصعب تحريك مقبضها."""
 
 JOB_CONFIRMATION = "السيد محمد في انتظارك!"
-
 CUSTOMER_CONFIRM = "job confirm"
-
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+COUNT = 0
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+  return "Bla Bla"
 
 @app.route("/send")
 def send_sms():
   global client
   print 'Sending a message...'
   new_message = client.messages.create(to=PHONE, from_='+19143025185', body=JOB_REQUEST)
-  return "Hello World!"
+  return "Thank you!"
 
 @app.route("/receive_sms")
 def receive_sms():
-  print 'RECEIVED SMS!'
-  return "Received SMS!!!"
+  if request.args['Body'] == 'Y':
+    new_message = client.messages.create(to=PHONE, from_='+19143025185', body=JOB_DETAILS)
+  elif request.args['Body'] == 'Ok':
+    new_message_1 = client.messages.create(to=PHONE, from_='+19143025185', body=JOB_CONFIRMATION)
+    new_message_2 = client.messages.create(to=CUSTOMER, from_='+19143025185', body=CUSTOMER_CONFIRM)
+  else:
+    new_message_1 = client.messages.create(to=PHONE, from_='+19143025185', body='OK')
+    new_message_2 = client.messages.create(to=CUSTOMER, from_='+19143025185', body='Job Done. 50USD debited from your credit card.')	#should be in turkish
+  return "OK!"
 
+  
 
 if __name__ == "__main__":
   app.run()
